4ad0ad4bb0fe9f9880a1af1a2aa8d378e60d9ba9
rlaehgus97 <starkim625@gmail.com>
2024-07-16 17:06:32 +0900

    수정

--- src/shipzo_plus/shipzo_plus.py
+++ src/shipzo_plus/shipzo_plus.py
@@ -1,9 +1,7 @@
 #더하기
 
-def sum(a,b):
+def sum():
+    input_a = int(input('input number A: '))
+    input_b = int(input('input number B: '))
     print(a+b)
 
-input_a = int(input('input number A: '))
-input_b = int(input('input number B: '))
-
-sum(input_a, input_b)
